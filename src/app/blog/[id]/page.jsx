15cf91b925957686876551d190d48e586650d036
Sergi-Virgili <svirgilif@gmail.com>
2023-09-25 20:50:09 +0200

    add more posts in post detail loading use suspense

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { Suspense } from "react";
+import Blog from "../page";
+import "./post-detail.css";
 
 async function getPostById(id) {
   return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
@@ -10,9 +12,18 @@ export default async function Post({ params }) {
   const post = await getPostById(params.id);
 
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <p>{post.body}</p>
+    <div className="post_detail">
+      <section>
+        <h1 className="post_detail-title">{post.title}</h1>
+        <p className="post_detail-text">{post.body}</p>
+      </section>
+      <hr />
+      <section>
+        <h2>More posts</h2>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Blog />
+        </Suspense>
+      </section>
     </div>
   );
 }
