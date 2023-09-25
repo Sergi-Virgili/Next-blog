d1903706d8d4bf26efaab4ff7e37b9e7456c33ba
Sergi-Virgili <svirgilif@gmail.com>
2023-09-25 19:42:08 +0200

    add link post-card to post detail url

--- src/app/blog/components/post-card.jsx
+++ src/app/blog/components/post-card.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./post-card.css";
+import Link from "next/link";
 
 export function PostCard({ post }) {
   return (
-    <article className="post_card">
-      <h2 className="post_card-title">{post.title}</h2>
-      <p className="post_card-body">{post.body}</p>
-    </article>
+    <Link href={`/blog/${post.id}`}>
+      <article className="post_card">
+        <h2 className="post_card-title">{post.title}</h2>
+        <p className="post_card-body">{post.body}</p>
+      </article>
+    </Link>
   );
 }
